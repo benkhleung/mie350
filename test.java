d2686eac02d2b186b038939ccf299d802e033f78
benkhleung <benkh.leung@mail.utoronto.ca>
2014-10-12 00:04:28 -0400

    test java program
    just trying this out.

---
+++ test.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<Integer> x = new ArrayList<Integer>();
+		for (int k=1; k<=15; k+=2) x.add(k);
+		for (int k=1; k<5; k++) x.remove(k);
+		for (int k : x) System.out.print(k + "-");
+		
+		
+	}
+
+}
