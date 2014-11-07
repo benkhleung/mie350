4c3cf6269eba435d085a903dc096c7bfbeddf33f
slooshieHorse <welapapa7@gmail.com>
2014-11-07 01:13:23 -0500

    Create Stores.java

---
+++ Stores.java
@@ -0,0 +1,22 @@
+package com.mie.model;
+
+//only getters
+
+public class Stores {
+	private int storeID;
+	private String storeName;
+	private String url;
+	
+	public int getStoreID(){
+		return storeID;
+	}
+	
+	public String getStoreName(){
+		return storeName;
+	}
+	
+	public String getURL(){
+		return url;
+	}
+	
+}
