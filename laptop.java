711512e573d8ce011d7765c09319584e5c814056
slooshieHorse <welapapa7@gmail.com>
2014-11-07 01:12:40 -0500

    laptop.java
    for model

---
+++ laptop.java
@@ -0,0 +1,162 @@
+  package com.mie.model;
+
+//getters
+
+public class Laptop {
+	private int productID;
+	private String brand;
+	private String modelName;
+	private String modelNo;
+	private String category;
+	private String price;
+	private String priceRange;
+	private String colour;
+	private String screenSize;
+	private String dimensions;
+	private String weight;
+	private String weightRange;
+	private String cpu;
+	private String cpuBrand;
+	private String os;
+	private String graphics;
+	private String graphicsBrand;
+	private String ram;
+	private String hdd;
+	private String ssd;
+	private String dvdDrive;
+	private String bluetooth;
+	private String usbPorts;
+	private String internet;
+	private String batteryLife;
+	private String batteryLifeRange;
+	private String warranty;
+	private String displayPorts;
+	private String camera;
+	private String backlitKeyboard;
+	private String imgSource;
+	
+	public int getProductID(){
+		return productID;
+	}
+	
+	public String getBrand(){
+		return brand;
+	}
+	
+	public String getModelName(){
+		return modelName;
+	}
+	
+	public String getModelNo(){
+		return modelNo;
+	}
+	
+	public String getCategory(){
+		return category;
+	}
+	
+	public String getPrice(){
+		return price;
+	}
+	
+	public String getPriceRange(){
+		return priceRange;
+	}
+	
+	public String getColour(){
+		return colour;
+	}
+	
+	public String getScreenSize(){
+		return screenSize;
+	}
+	
+	public String getDimensions(){
+		return dimensions;
+	}
+	
+	public String getWeight(){
+		return weight;
+	}
+	
+	public String getWeightRange(){
+		return weightRange;
+	}
+	
+	public String getCpu(){
+		return cpu;
+	}
+	
+	public String getCpuBrand(){
+		return cpuBrand;
+	}
+	
+	public String getOs(){
+		return os;
+	}
+	
+	public String getGraphics(){
+		return graphics;
+	}
+	
+	public String getGraphicsBrand(){
+		return graphicsBrand;
+	}
+	
+	public String getRam(){
+		return ram;
+	}
+	
+	public String getHdd(){
+		return hdd;
+	}
+	
+	public String getSsd(){
+		return ssd;
+	}
+	
+	public String getDvdDrive(){
+		return dvdDrive;
+	}
+	
+	public String getBluetooth(){
+		return bluetooth;
+	}
+	
+	public String getUsbPorts(){
+		return usePorts;
+	}
+	
+	public String getInternet(){
+		return internet;
+	}
+	
+	public String getBatteryLife(){
+		return batteryLife;
+	}
+	
+	public String getBatteryLifeRange(){
+		return batteryLifeRange;
+	}
+	
+	public String getWarranty(){
+		return warranty;
+	}
+	
+	public String getDisplayPorts(){
+		return displayPorts;
+	}
+	
+	public String getCamera(){
+		return camera;
+	}
+	
+	public String getBacklitKeyboard(){
+		return backlitKeyboard;
+	}
+	
+	public String getImgSource(){
+		return imgSource;
+	}
+	
+}
